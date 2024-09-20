4cd1d3666e4c9bdc30892502fe83a70fc31c0ed8
basche1979 <info@marc79.de>
2024-09-20 07:01:08 +0200

    MacBook v-row und v-column Test

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Logout from "@/components/Logout.vue";
 import Profile from "@/components/Profile.vue";
 import ResetPassword from "@/components/ResetPassword.vue";
 import ResetPasswordRequest from "@/components/ResetPasswordRequest.vue";
+import FlexTest from "@/components/FlexTest.vue";
 
 const routes = [
     {
@@ -75,6 +76,12 @@ const routes = [
         name: 'reset-password-request',
         component: ResetPasswordRequest,
 
+    },
+    {
+        path: '/flextest',
+        name: 'flextest',
+        component: FlexTest,
+        meta: { requiresAuth: true }, // Markiere diese Route als geschützt
     },
 
 ];
