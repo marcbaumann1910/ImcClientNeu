acca487be614b39f8087f2f952546d150b45696e
basche1979 <info@marc79.de>
2024-11-07 18:52:33 +0100

    MacBook LeihvorgangVerwalten Responsive versuch, muss weiter optimiert werden

--- src/components/LeihvorgangVerwalten/LeihvorgangVerwalten.vue
+++ src/components/LeihvorgangVerwalten/LeihvorgangVerwalten.vue
@@ -177,243 +177,218 @@ function showDialogArtikelTausch(item, member){
 </script>
 
 <template>
+  <v-container fluid>
+    <!-- Überschrift -->
+    <v-card class="mb-4">
+      <v-card-text class="text-center">
+        <v-card-title>Hier können Sie die Leihvorgänge verwalten</v-card-title>
+      </v-card-text>
+    </v-card>
 
-  <v-col cols="12" md="8" lg="8">
+    <!-- Suchfeld für Mitglieder -->
+    <v-text-field
+        v-model="searchMitglied"
+        class="mb-4 search-field"
+        label="Suche Mitglieder"
+        prepend-inner-icon="mdi-magnify"
+        clearable
+        @click:clear=""
+        variant="solo-filled"
+        hide-details
+        single-line
+    ></v-text-field>
 
-  <v-container>
-    <div>
-      <v-card class="d-flex justify-center mb-10">
-        <v-card-item>
-          <v-card-title>Hier können Sie die Leihvorgänge verwalten</v-card-title>
-        </v-card-item>
-      </v-card>
-    </div>
-
-
-    <div>
-      <v-text-field
-          v-model="searchMitglied"
-          class="search-field mb-4"
-          label="Suche Mitglieder"
-          prepend-inner-icon="mdi-magnify"
-          clearable
-          @click:clear=""
-          variant="solo-filled"
-          hide-details
-          single-line
-      ></v-text-field>
-    </div>
-
-<!--    <div>-->
-<!--      <v-card class="mb-10 d-flex align-center">-->
-<!--        <v-card-title>Filter:</v-card-title>-->
-<!--        <v-chip color="orange" outlined text="offene Vorgänge" class="mr-3"></v-chip>-->
-<!--        <v-chip color="green" outlined text="abgeschlossen Vorgänge"></v-chip>-->
-<!--      </v-card>-->
-<!--    </div>-->
-
-
-    <div>
-      <v-expansion-panels v-model="expandedPanels" multiple>
-        <!-- Erstes Level -->
-        <v-expansion-panel
-            v-for="(item) in filteredLeihvorgaengeMitglieder"
-            :key="item.easyVereinMitglied_id"
-            :value="item.easyVereinMitglied_id"
-            class="mb-1">
-          <v-expansion-panel-title @click="expansionForLeihvorgang(item)">
-            {{ item.easyVereinMitglied_firstName }} {{ item.easyVereinMitglied_familyName }}
-            <template v-slot:actions="{ expanded }">
-              <v-icon :color="!expanded ? 'orange' : ''"
-                      :icon="expanded ? 'mdi-pencil' : 'mdi-progress-clock'"></v-icon>
-            </template>
-          </v-expansion-panel-title>
-          <v-divider></v-divider>
-          <div class="d-flex justify-start">
-            <!-- Checkbox wird nur angezeigt, wenn das Panel erweitert ist -->
-            <!-- Siehe ausführliche Beschreibung in der Doku -->
-            <v-checkbox
-                class="ml-3 mt-3"
-                color="orange"
-                label="ausgeliehen"
-                hide-details
-                v-model="checkedItems[item.easyVereinMitglied_id].ausgeliehen"
-                @change="handleCheckboxAusgeliehen(item)"
-                v-if="expandedPanels.includes(item.easyVereinMitglied_id)"
-            ></v-checkbox>
-            <v-checkbox
-                class="mt-3"
-                color="green"
-                label="abgeschlossen"
-                hide-details
-                v-model="checkedItems[item.easyVereinMitglied_id].abgeschlossen"
-                @change="handleCheckboxAbgeschlossen(item)"
-                v-if="expandedPanels.includes(item.easyVereinMitglied_id)"
-            ></v-checkbox>
-            <v-spacer></v-spacer>
-            <v-text-field
-                v-model="searchArtikels[item.easyVereinMitglied_id]"
-                :loading="loading"
-                append-inner-icon="mdi-magnify"
-                density="compact"
-                label="Artikel suchen"
-                variant="solo"
-                single-line
-                class="mt-6"
-                clearable
-                v-if="expandedPanels.includes(item.easyVereinMitglied_id)"
-            ></v-text-field>
-
-            <v-spacer></v-spacer>
-          </div>
-
-
-          <v-divider></v-divider>
-          <v-expansion-panel-text>
-            <!-- ###################### Details ############################## -->
+    <!-- Expansion Panels -->
+    <v-expansion-panels v-model="expandedPanels" multiple>
+      <!-- Erstes Level -->
+      <v-expansion-panel
+          v-for="(item) in filteredLeihvorgaengeMitglieder"
+          :key="item.easyVereinMitglied_id"
+          :value="item.easyVereinMitglied_id"
+          class="mb-2"
+      >
+        <!-- Panel Titel -->
+        <v-expansion-panel-title
+            @click="expansionForLeihvorgang(item)"
+            v-slot="{ expanded }"
+        >
+          <div class="d-flex justify-space-between align-center">
             <div>
-              <v-list>
-                <v-row>
-                  <v-col>
-                    <v-list-item-action class="ml-4">
-                      <v-list-item-title>Artikel {{ item.anzahlArtikel }} Stück</v-list-item-title>
-                    </v-list-item-action>
-                  </v-col>
-
-                  <v-list-item-action class="mr-7">
-                    <v-list-item-title>Preis</v-list-item-title>
-                  </v-list-item-action>
-
-                </v-row>
-                <v-divider></v-divider>
-                <v-list-item
-                    v-for="(itemArtikelDetails) in filteredArtikelDetails(item)"
-                    :key="itemArtikelDetails.IDinventarBuchungenPositionen"
-                    class="mb-2 mt-2">
-                  <v-row no-gutters align="start" class="w-100">
-                    <v-col cols="1" class="d-flex flex-column justify-center align-start align-self-stretch">
-                      <!-- Flexibel halten und rechten Abstand hinzufügen -->
-                      <v-icon
-                          class="ml-6"
-                          :color="isVisibleIventarStatus(itemArtikelDetails.ibp_IDinventarBuchungenPositionenStatus) ? 'orange' : 'green'"
-                      >
-                        {{
-                          isVisibleIventarStatus(itemArtikelDetails.ibp_IDinventarBuchungenPositionenStatus) ? 'mdi-share' : 'mdi-lock'
-                        }}
-                      </v-icon>
-                      <v-img
-                          :src="`${imageUrl}${itemArtikelDetails.ia_Bildpfad}`"
-                          alt="Artikelbild"
-                          min-height="70"
-                          min-width="70"
-                          max-width="70"
-                          max-height="70"
-                          class="mt-2 mb-4 ml-1"
-                      ></v-img>
-                    </v-col>
-
-                    <v-col cols="3" class="mb-2">
-                      <v-list-item-title>{{ itemArtikelDetails.ia_ArtikelBezeichnung }}</v-list-item-title>
-                      <v-list-item-title>Nummer: {{ itemArtikelDetails.ibp_externeInventarNummer }}</v-list-item-title>
-                      <v-list-item-subtitle>Farbe: {{ itemArtikelDetails.farbe }}</v-list-item-subtitle>
-                      <v-list-item-subtitle>Größe: {{
-                          itemArtikelDetails.konfektionsGroesse_Konfektionsgroesse
-                        }}
-                      </v-list-item-subtitle>
-                      <v-list-item-subtitle>Menge: {{ itemArtikelDetails.ibp_Menge }} Stück</v-list-item-subtitle>
-                      <v-list-item-subtitle>
-                        Status: {{ itemArtikelDetails.ibp_Bezeichnung }}
-
-                      </v-list-item-subtitle>
-                      <v-list-item-subtitle>Datum: {{
-                          formatDate(itemArtikelDetails.ibp_StatusDatum)
-                        }}
-                      </v-list-item-subtitle>
-
-
-                    </v-col>
-
-                    <v-col cols="2" class="d-flex flex-column justify-end align-start align-self-stretch mb-2">
-
-                      <v-label
-                          class="mb-6"
-                          v-if="!isVisibleIventarStatus(itemArtikelDetails.ibp_IDinventarBuchungenPositionenStatus)"
-                      >
-                        Zustand bei Rücknahme: {{ itemArtikelDetails.iz_Bezeichnung }}
-                      </v-label>
-
-                      <v-label
-                          class="mb-6"
-                          v-if="!isVisibleIventarStatus(itemArtikelDetails.ibp_IDinventarBuchungenPositionenStatus)"
-                      >
-                        Bemerkung: {{ itemArtikelDetails.ibp_Bemerkung }}
-                      </v-label>
-
-                      <v-label
-                          @click="showDialogRuecknahme(itemArtikelDetails, item)"
-                          class="hover text-subtitle-2"
-                          v-if="isVisibleIventarStatus(itemArtikelDetails.ibp_IDinventarBuchungenPositionenStatus)"
-                      >
-                        <v-icon class="mr-1">mdi-arrow-down-thin-circle-outline</v-icon>
-                        Rücknahme
-                      </v-label>
-
-                    </v-col>
-
-                    <v-col cols="2" class="d-flex flex-column justify-end align-start align-self-stretch mb-2">
-                      <v-label
-                          @click="showDialogNummerAendern(itemArtikelDetails.ibp_IDinventarBuchungenPositionen, item)"
-                          class="hover text-subtitle-2"
-                          v-if="isVisibleIventarStatus(itemArtikelDetails.ibp_IDinventarBuchungenPositionenStatus)"
-                      >
-                        <v-icon class="mr-1">mdi-pencil-outline</v-icon>
-                        Nummer ändern
-                      </v-label>
-                    </v-col>
-
-                    <v-col cols="2" class="d-flex flex-column justify-end align-start align-self-stretch mb-2">
-                      <v-label
-                          @click="showDialogArtikelTausch(itemArtikelDetails, item)"
-                          class="hover text-subtitle-2 text-blue-darken-4 mt-2"
-                          v-if="isVisibleIventarStatus(itemArtikelDetails.ibp_IDinventarBuchungenPositionenStatus)"
-                      >
-                        <v-icon class="mr-1">mdi-sync</v-icon>
-                        Artikel austauschen
-                      </v-label>
-                    </v-col>
-
-                    <v-col cols="2" class="d-flex justify-end align-end">
-                      <v-list-item-title>{{ Math.round(((itemArtikelDetails.ibp_Preis) * 100) / 100).toFixed(2) }} €
-                      </v-list-item-title>
-                    </v-col>
-
-                    <v-divider></v-divider>
-
-                  </v-row>
-                </v-list-item>
-
-                <v-row>
-                  <v-col class="d-flex justify-end mr-0">
-                    <v-list-item-action class="mr-2">
-                      <v-list-item-title><b>Gesamtpreis: {{ (Math.round(item.gesamtPreis * 100) / 100).toFixed(2) }}
-                        €</b></v-list-item-title>
-                    </v-list-item-action>
-                  </v-col>
-
-                </v-row>
-              </v-list>
+              {{ item.easyVereinMitglied_firstName }} {{ item.easyVereinMitglied_familyName }}
             </div>
-          </v-expansion-panel-text>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </div>
-    <DialogRuecknahme v-if="showDialogRuecknahme" :member="selectedMember" />
-    <DialogNummerAendern :member="selectedMember"/>
-    <DialogArtikelTausch :member="selectedMember"/>
+            <v-icon :color="expanded ? 'primary' : 'grey'">
+              {{ expanded ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
+            </v-icon>
+          </div>
+        </v-expansion-panel-title>
 
+        <!-- Panel Inhalt -->
+        <v-expansion-panel-text>
+          <!-- Filter und Suchfeld -->
+          <v-row class="mb-4">
+            <v-col cols="12" sm="6" class="d-flex align-center">
+              <v-row>
+                <v-col class="d-flex flex-column align-center">
+                  <v-checkbox
+                      color="orange"
+                      hide-details
+                      v-model="checkedItems[item.easyVereinMitglied_id].ausgeliehen"
+                      @change="handleCheckboxAusgeliehen(item)"
+                      class="ma-0 pa-0"
+                      false-value="ausgeliehen"
+                  ></v-checkbox>
+                  <span class="text-center">Ausgeliehen</span>
+                </v-col>
+                <v-col class="d-flex flex-column align-center">
+                  <v-checkbox
+                      color="green"
+                      hide-details
+                      v-model="checkedItems[item.easyVereinMitglied_id].abgeschlossen"
+                      @change="handleCheckboxAbgeschlossen(item)"
+                      class="ma-0 pa-0"
+                      false-value="abgeschlossen"
+                  ></v-checkbox>
+                  <span class="text-center">Abgeschlossen</span>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field
+                  v-model="searchArtikels[item.easyVereinMitglied_id]"
+                  :loading="loading"
+                  append-inner-icon="mdi-magnify"
+                  density="compact"
+                  label="Artikel suchen"
+                  variant="solo"
+                  single-line
+                  class="mt-0"
+                  clearable
+                  hide-details
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <!-- Artikelliste -->
+          <v-list>
+            <!-- Überschriften -->
+            <v-list-item>
+              <v-row class="w-100">
+                <v-col cols="6">
+                  <v-list-item-title>Artikel ({{ item.anzahlArtikel }})</v-list-item-title>
+                </v-col>
+                <v-col cols="6" class="text-right">
+                  <v-list-item-title>Preis</v-list-item-title>
+                </v-col>
+              </v-row>
+            </v-list-item>
+            <v-divider></v-divider>
+
+            <!-- Artikel Details -->
+            <v-list-item
+                v-for="(itemArtikelDetails) in filteredArtikelDetails(item)"
+                :key="itemArtikelDetails.IDinventarBuchungenPositionen"
+                class="mb-2 mt-2"
+            >
+              <v-row no-gutters align="start" class="w-100">
+                <!-- Icon und Bild -->
+                <v-col cols="2" sm="1" class="d-flex flex-column align-center">
+                  <v-icon
+                      :color="isVisibleIventarStatus(itemArtikelDetails.ibp_IDinventarBuchungenPositionenStatus) ? 'orange' : 'green'"
+                  >
+                    {{
+                      isVisibleIventarStatus(itemArtikelDetails.ibp_IDinventarBuchungenPositionenStatus)
+                          ? 'mdi-share'
+                          : 'mdi-lock'
+                    }}
+                  </v-icon>
+                  <v-img
+                      :src="`${imageUrl}${itemArtikelDetails.ia_Bildpfad}`"
+                      alt="Artikelbild"
+                      max-width="60"
+                      max-height="60"
+                      class="mt-2 mb-2"
+                  ></v-img>
+                </v-col>
+
+                <!-- Artikelinformationen -->
+                <v-col cols="10" sm="5" class="mb-2">
+                  <v-list-item-title>{{ itemArtikelDetails.ia_ArtikelBezeichnung }}</v-list-item-title>
+                  <v-list-item-subtitle>
+                    Nummer: {{ itemArtikelDetails.ibp_externeInventarNummer }}
+                  </v-list-item-subtitle>
+                  <v-list-item-subtitle>Farbe: {{ itemArtikelDetails.farbe }}</v-list-item-subtitle>
+                  <v-list-item-subtitle>
+                    Größe: {{ itemArtikelDetails.konfektionsGroesse_Konfektionsgroesse }}
+                  </v-list-item-subtitle>
+                  <v-list-item-subtitle>Menge: {{ itemArtikelDetails.ibp_Menge }} Stück</v-list-item-subtitle>
+                  <v-list-item-subtitle>Status: {{ itemArtikelDetails.ibp_Bezeichnung }}</v-list-item-subtitle>
+                  <v-list-item-subtitle>Datum: {{ formatDate(itemArtikelDetails.ibp_StatusDatum) }}</v-list-item-subtitle>
+                </v-col>
+
+                <!-- Zustand und Bemerkung -->
+                <v-col cols="12" sm="6" class="d-flex flex-column mb-2">
+                  <div v-if="!isVisibleIventarStatus(itemArtikelDetails.ibp_IDinventarBuchungenPositionenStatus)">
+                    <v-label class="mb-2">
+                      Zustand bei Rücknahme: {{ itemArtikelDetails.iz_Bezeichnung }}
+                    </v-label>
+                    <v-label class="mb-2">Bemerkung: {{ itemArtikelDetails.ibp_Bemerkung }}</v-label>
+                  </div>
+
+                  <!-- Aktionen -->
+                  <div v-else class="d-flex flex-wrap">
+                    <v-label
+                        @click="showDialogRuecknahme(itemArtikelDetails, item)"
+                        class="hover text-subtitle-2 mr-4 mb-2"
+                    >
+                      <v-icon class="mr-1">mdi-arrow-down-thin-circle-outline</v-icon> Rücknahme
+                    </v-label>
+                    <v-label
+                        @click="showDialogNummerAendern(itemArtikelDetails.ibp_IDinventarBuchungenPositionen, item)"
+                        class="hover text-subtitle-2 mr-4 mb-2"
+                    >
+                      <v-icon class="mr-1">mdi-pencil-outline</v-icon> Nummer ändern
+                    </v-label>
+                    <v-label
+                        @click="showDialogArtikelTausch(itemArtikelDetails, item)"
+                        class="hover text-subtitle-2 text-blue-darken-4 mb-2"
+                    >
+                      <v-icon class="mr-1">mdi-sync</v-icon> Artikel austauschen
+                    </v-label>
+                  </div>
+                </v-col>
+
+                <!-- Preis -->
+                <v-col cols="12" class="text-right">
+                  <v-list-item-title>
+                    {{ (Math.round(itemArtikelDetails.ibp_Preis * 100) / 100).toFixed(2) }} €
+                  </v-list-item-title>
+                </v-col>
+              </v-row>
+              <v-divider></v-divider>
+            </v-list-item>
+
+            <!-- Gesamtpreis -->
+            <v-list-item>
+              <v-row class="w-100">
+                <v-col cols="12" class="text-right">
+                  <v-list-item-title>
+                    <b>Gesamtpreis: {{ (Math.round(item.gesamtPreis * 100) / 100).toFixed(2) }} €</b>
+                  </v-list-item-title>
+                </v-col>
+              </v-row>
+            </v-list-item>
+          </v-list>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+    <!-- Dialoge -->
+    <DialogRuecknahme v-if="showDialogRuecknahme" :member="selectedMember" />
+    <DialogNummerAendern :member="selectedMember" />
+    <DialogArtikelTausch :member="selectedMember" />
   </v-container>
-  </v-col>
 </template>
+
 
 <style scoped>
 
