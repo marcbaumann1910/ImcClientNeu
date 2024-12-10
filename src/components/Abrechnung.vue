1ed63e1a90b0f6690a77035bb965a906a0d7ff77
basche1979 <info@marc79.de>
2024-12-10 17:09:43 +0100

    Ubuntu: Änderung Abrechnung

--- src/components/Abrechnung.vue
+++ src/components/Abrechnung.vue
@@ -1,5 +1,20 @@
 <template>
   <h1>Abrechnung</h1>
+  <v-container>
+
+    <v-list>
+
+      <v-list-item>
+        <v-list-item-title>Title</v-list-item-title>
+      </v-list-item>
+
+    </v-list>
+
+
+  </v-container>
+
+
+
   </template>
 
 <script setup>
