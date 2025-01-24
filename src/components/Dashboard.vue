4d26a8e4958cbb377b99a593fd5947d92c4fc0b0
basche1979 <info@marc79.de>
2025-01-24 17:58:50 +0100

    Ubuntu: Dashboard: Backend-Daten Empfang überarbeitet und aktuellste Leihvorgänge anzeigen

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,20 +3,27 @@ import {computed, ref} from 'vue'
 import PieChart from '@/components/PieChart.vue'
 import { onMounted } from "vue";
 import AuthenticationService from "@/services/AuthenticationService.js";
+import {formatDate} from "@/scripte/globalFunctions.js";
+import * as globalFunction from "../scripte/globalFunctions.js";
 const chartDataBackend = ref([]);
+const neuesteVorgaenge = ref([]);
+const lagerbestand = ref([]);
 
 onMounted(async () => {
   try {
 
+    //Vom Backend werden 'leihvorgaenge', 'neuesteVorgaenge', 'lagerbestand' abgerufen
     const response = await AuthenticationService.dashboardLeihvorgaenge()
     //Daten vom Backend --> Anzahl in eine Zahl wandeln, damit dies in Prozent umgerechnet werden können
-    chartDataBackend.value = response.data.map(item => {
+    chartDataBackend.value = response.data.leihvorgaenge.map(item => {
       return {
         Bezeichnung: item.Bezeichnung,
         Anzahl: parseFloat(item.Anzahl),
       }
     })
-    console.log('charDataBackend', chartDataBackend.value)
+    neuesteVorgaenge.value = response.data.neuesteVorgaenge
+    lagerbestand.value = response.data.lagerbestand
+
     console.log('Dashboard Leihvoränge erfolgreich abrufen')
   }catch (err) {
     console.log('FEHLER: Dashboard Leihvoränge', err.message)
@@ -55,7 +62,7 @@ const chartData = computed(() => {
 const chartOptions = ref({
   responsive: true,
   plugins: {
-    legend: { position: 'top' },
+    //legend: { position: 'top' },
     datalabels: {
       color: '#fff',
       font: { weight: 'bold' },
@@ -74,12 +81,14 @@ const chartOptions = ref({
 </script>
 
 <template>
+<v-container>
+
   <div class="dashboard-background">
     <h1>Dashboard</h1>
-    <h5>Herzlich Willkommen</h5>
-    <v-container>
+  </div>
 
       <v-row>
+        <!--Leihvorgänge Pie (Kreisdiagramm)-->
         <v-col cols="12" sm="6" md="4">
           <v-card  max-width="500">
             <v-card-title>Leihvorgänge</v-card-title>
@@ -88,19 +97,55 @@ const chartOptions = ref({
         </v-col>
 
         <v-col cols="12" sm="6" md="4">
-          <v-card>
-            <v-card-title>Kachel 2</v-card-title>
+
+        <!--Aktuellsten Leihvorgänge-->
+          <v-card  max-width="500">
+            <v-card-title>Aktuellsten Leihvorgänge</v-card-title>
+
+            <v-list>
+              <v-list-item
+                  v-for="(item, index) in neuesteVorgaenge"
+                  :key="index"
+              >
+                <v-row no-gutters align="start">
+                  <v-col cols="12" sm="6" md="4">
+                    <v-list-item-title>
+                      {{ item.firstName }}
+                    </v-list-item-title>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-list-item-title>
+                      {{ item.familyName }}
+                    </v-list-item-title>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-list-item-title>
+                      {{ item.city }}
+                    </v-list-item-title>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-list-item-title>
+                      {{ globalFunction.formatDate(item.DatumBuchung) }}
+                    </v-list-item-title>
+                  </v-col>
+
+                </v-row>
+                <v-divider></v-divider>
+              </v-list-item>
+            </v-list>
           </v-card>
         </v-col>
 
+
+      </v-row>
+
+      <v-row>
         <v-col cols="12" sm="6" md="4">
           <v-card>
             <v-card-title>Kachel 3</v-card-title>
           </v-card>
         </v-col>
-      </v-row>
 
-      <v-row>
         <v-col cols="12" sm="6" md="4">
           <v-card>
             <v-card-title>Kachel 4</v-card-title>
@@ -110,7 +155,7 @@ const chartOptions = ref({
 
 
     </v-container>
-  </div>
+
 </template>
 
 <style scoped>
