d674f96f1341b04f439810402407a4c230775855
basche1979 <info@marc79.de>
2026-01-09 15:25:43 +0100

    Ubuntu: Mietvertrag confirm Umsetzung abgeschlossen!

--- src/components/MietvertragConfirm.vue
+++ src/components/MietvertragConfirm.vue
@@ -3,7 +3,10 @@ import {ref} from "vue";
 import AuthenticationService from "@/services/AuthenticationService.js";
 import { useRoute } from "vue-router";
 const router = useRoute();
-
+const btnDisabled = ref(false);
+const successMessage = ref("");
+const isVisible = ref(true);
+const confirmFromBackend = ref(false);
 
 const accepted = ref(false);
 
@@ -24,13 +27,17 @@ async function confirmMietvertrag() {
     });
 
     if(response.data.confirm === true) {
-      alert(response.data.message);
+      successMessage.value = response.data.message //Verarbeitet die Antwort vom Backend und gibt diese aus!
+      btnDisabled.value = true;
+      confirmFromBackend.value = true; //Wenn Backend den Token bestätigt hat, wird die Anzeige im Card-Item umgestellt
     }
     console.log("data", response.data);
   } catch (err) {
     const data = err?.response?.data;
     if (data?.confirm === false) {
       alert(data.message);
+      btnDisabled.value = true;
+      isVisible.value = false;
     }
 
     console.log("confirm error", err?.response?.status, err?.response?.data || err.message);
@@ -58,24 +65,32 @@ async function confirmMietvertrag() {
     </v-card-item>
 
     <v-card-text>
-      Hiermit bestätige ich den Erhalt und die Vertragsbedingungen aus dem mir vorliegenden Mietvertrag
+      Ich bestätige den Erhalt des Mietvertrags und akzeptiere die darin enthaltenen Vertragsbedingungen.
     </v-card-text>
 
+    <v-card-item class="pb-0">
+      <v-checkbox
+          v-if="!confirmFromBackend"
+          v-model="accepted"
+          label="Ich akzeptiere die Bedingungen"
+          class="my-0"
+          hide-details
+          density="compact"
+      />
+      <div v-else>{{ successMessage }}</div>
+    </v-card-item>
 
-    <v-card-item  class="d-flex justify-center">
+    <v-card-item class="pt-0 d-flex justify-center">
       <v-btn
           class="text-none text-subtitle-1"
           color="#5865f2"
           size="large"
           variant="flat"
+          :disabled="btnDisabled"
           @click="confirmMietvertrag"
       >
         Bestätigen
       </v-btn>
-    </v-card-item>
-
-    <v-card-item>
-      <v-checkbox v-model="accepted" label="Ich akzeptiere die Bedingungen" />
     </v-card-item>
 
 
